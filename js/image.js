a65b04cb2b0a61aa9c3bfbd61df4694b645920e4
Miyashooooo <cstrike.asia@gmail.com>
2025-01-25 02:18:21 +0800

    fix: image

--- js/image.js
+++ js/image.js
@@ -7,6 +7,10 @@ function load() {
     }`;
   });
   axios.get(`https://${URL_MAIN}/file/images/report/json/`).then((res) => {
+    res.data = res.data.map(item => ({
+      ...item,
+      filter: item.name.split('-').slice(1).join('-'),
+    })).sort((a, b) => a.filter.localeCompare(b.filter));
     document.getElementById(
       "report"
     ).src = `https://${URL_MAIN}/file/images/report/${res.data.at(-1).name}`;
